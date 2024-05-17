c1273eea1dc0e5708c2aa9cd6dcab253bf974fba
bono <shippingbok@gmail.com>
2024-05-17 12:57:09 +0900

    [💄UI]ColorSystem

--- Yanolja/Sources/DesignSystem/Extensions/ShapeStyle+Angel.swift
+++ Yanolja/Sources/DesignSystem/Extensions/ShapeStyle+Angel.swift
@@ -10,4 +10,34 @@ import SwiftUI
 
 extension ShapeStyle where Self == Color {
   static var angelGray: Color { .init(hex: 0xEEEEEF) }
+  
+  static var doosan1: Color { .init(hex: 0x03234C) }
+  static var doosan2: Color { .init(hex: 0xE22930) }
+  
+  static var lotte1: Color { .init(hex: 0x62B0E1) }
+  static var lotte2: Color { .init(hex: 0xD00E31) }
+  
+  static var samsung1: Color { .init(hex: 0x0065B6) }
+  static var samsung2: Color { .init(hex: 0xBDC2C4) }
+  
+  static var kiwoom1: Color { .init(hex: 0x820024) }
+  static var kiwoom2: Color { .init(hex: 0xEC008F) }
+  
+  static var hanhwa1: Color { .init(hex: 0xF37321) }
+  static var hanhwa2: Color { .init(hex: 0xFFFFFF) }
+  
+  static var lg1: Color { .init(hex: 0xC30136) }
+  static var lg2: Color { .init(hex: 0xFFFFFF) }
+  
+  static var nc1: Color { .init(hex: 0x1F477A) }
+  static var nc2: Color { .init(hex: 0xC59F7B) }
+  
+  static var ssg1: Color { .init(hex: 0xCE142C) }
+  static var ssg2: Color { .init(hex: 0xFDBB2F) }
+  
+  static var kt1: Color { .init(hex: 0xEA0029) }
+  static var kt2: Color { .init(hex: 0x05141F) }
+  
+  static var kia1: Color { .init(hex: 0x221E1F) }
+  static var kia2: Color { .init(hex: 0xED1B23) }
 }
---
+++ Yanolja/Sources/DesignSystem/Views/Main/MyCharacterView.swift
@@ -0,0 +1,19 @@
+//
+//  MyCharacterView.swift
+//  Yanolja
+//
+//  Created by 박혜운 on 5/17/24.
+//  Copyright © 2024 com.mc2.yanolja. All rights reserved.
+//
+
+import SwiftUI
+
+struct MyCharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyCharacterView()
+}
