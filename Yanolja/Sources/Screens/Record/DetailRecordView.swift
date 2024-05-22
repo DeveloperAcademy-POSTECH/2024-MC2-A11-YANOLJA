40f1a9d54d62ad720dbd3c7fa479e4c9149f134a
Guisik Han <guisikhan@10-141-55-130.0200.pos.kr.iosda.org>
2024-05-22 19:26:42 +0900

    [💄UI] DetailRecordView 수정

--- Yanolja/Sources/Screens/Record/DetailRecordView.swift
+++ Yanolja/Sources/Screens/Record/DetailRecordView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 
+// 내가 보이는 뷰를 생성하거나 편집하는 enum
 enum RecordViewEditType {
   case create
   case edit
@@ -15,93 +16,138 @@ enum RecordViewEditType {
 
 // MARK: - 고스트 꺼
 struct DetailRecordView: View {
-  let editType: RecordViewEditType
+  @Environment (\.dismiss) var dismiss
+  var usecase: RecordUseCase
+  @State var recording: GameRecordModel // 수정할 때
+  private let editType: RecordViewEditType
   
-  
-  @State private var date = Date()
-  @State var selectedGame = ""
-  @State var selectedMyTeam = ""
-  @State var selectedYourTeam = ""
-  @State var selectedStadium = "잠실"
-  var gameResults = ["승리", "패배", "무승부"]
-  var allTeams = ["두산 베어스", "롯데 자이언츠", "삼성 라이온즈", "키움 히어로즈" ,"한화 이글스", "LG 트윈스", "NC 다이노스", "SSG 랜더스", "KIA 타이거즈", "KT 위즈"]
-  var stadiums = ["고척스카이돔","광주기아챔피언스필드", "대구삼성라이온즈파크", "대전한화생명이글스파크","사직야구장", "수원KT위즈파크", "인천SSG랜더스필드" ,"잠실야구장", "창원NC파크", "포항야구장"]
-  
-  
-  init(to editType: RecordViewEditType) {
+  init(
+    to editType: RecordViewEditType,
+    record: GameRecordModel = .init(),
+    usecase: RecordUseCase
+  ) {
     self.editType = editType
+    self.recording = record
+    self.usecase = usecase
   }
   
   var body: some View {
-    List() {
-      Section(
-        "직관 정보",
-        content: {
-          // DatePicker 사용
-          selectDate
-          // 나의 팀, 상대팀 Picker
-          HStack(spacing: 10) {
-            SelectTeamView(
-              type: .my,
-              selectedTeam: .doosan,
-              tappedAction: { selectedTeam in
-                print("\(selectedTeam)되잖아")
-              }
-            )
-            
-            Text("VS")
-              .font(.title2)
-              .foregroundStyle(.gray)
-            
-            SelectTeamView(
-              type: .vs,
-              selectedTeam: .doosan,
-              tappedAction: { selectedTeam in
-                print("\(selectedTeam)되잖아")
-                
-              }
-            )
-          }
-          // 경기장 Picker
-          Picker("경기장", selection: $selectedStadium) {
-            ForEach(stadiums, id: \.self) {
-              Text($0)
+    NavigationStack {
+      List() {
+        Section(
+          "직관 정보",
+          content: {
+            selectDate
+
+            HStack(spacing: 10) {
+              SelectTeamView(
+                type: .my,
+                selectedTeam: recording.myTeam,
+                tappedAction: { selectedTeam in
+                  recording.myTeam = selectedTeam
+                }
+              )
+              
+              Text("VS")
+                .font(.title2)
+                .foregroundStyle(.gray)
+              
+              SelectTeamView(
+                type: .vs,
+                selectedTeam: recording.vsTeam,
+                tappedAction: { selectedTeam in
+                  recording.vsTeam = selectedTeam
+                }
+              )
             }
-          }
-          .pickerStyle(.menu)
-        }
-      )
-      // 직관 결과 선택 picker
-      Picker("직관 결과", selection: $selectedGame){
-        ForEach(gameResults, id: \.self) {
-          Text($0)
-        }
-      }
-      .pickerStyle(.inline)
-      
-      
-      if editType == .edit {
-        Button(
-          action: {
-            print("고스트 바보")
-          },
-          label: {
-            Text("삭제")
+            // 경기장 Picker
+            Picker(
+              "경기장",
+              selection: $recording.stadiums
+            ) {
+              ForEach(BaseballStadiums.allCases, id: \.self) {
+                Text($0.name)
+              }
+            }
+            .pickerStyle(.menu)
           }
         )
+        // 직관 결과 선택 picker
+        Picker("직관 결과", selection: $recording.result) {
+          ForEach(GameResult.allCases, id: \.self) {
+            Text($0.label)
+          }
+        }
+        .pickerStyle(.inline)
+        
+        
+        if editType == .edit {
+          Button(
+            action: {
+              usecase.effect(.tappedDeleteRecord(recording.id))
+            },
+            label: {
+              Text("삭제")
+            }
+          )
+        }
       }
+      .toolbar(
+        content: {
+          ToolbarItem(placement: .topBarTrailing) {
+            Button(
+              action: {
+                if editType == .create { // 생성 시
+                  usecase.effect(.tappedSaveNewRecord(recording))
+                } else { // 수정 시
+                  usecase.effect(.tappedEditNewRecord(recording))
+                  dismiss()
+                }
+              },
+              label: {
+                Text("완료")
+              }
+            )
+          }
+          ToolbarItem(placement: .topBarLeading) {
+            Button(
+              action: {
+                if editType == .create { // 생성 시
+                  usecase.effect(.tappedCreateRecordSheet(false))
+                } else { // 수정 시
+                  usecase.effect(.tappedRecordCellToEditRecordSheet(false))
+                  dismiss()
+                }
+              },
+              label: {
+                Text("취소")
+                  .foregroundStyle(.red)
+              }
+            )
+          }
+        }
+      )
+      .navigationTitle("오늘의 직관")
+      .navigationBarTitleDisplayMode(.inline)
     }
+    
   }
   
   var selectDate: some View {
     DatePicker(
       "날짜",
-      selection: $date,
+      selection: $recording.date,
       displayedComponents: [.date]
     )
   }
 }
 
+// 화면을 보이는 뷰
 #Preview {
-  DetailRecordView(to: .edit)
+  DetailRecordView(
+    // 현재 보여지는 뷰는 편집(삭제 버튼 추가)
+    to: .edit,
+    // usecase는 RecordUseCase를 사용
+    usecase: RecordUseCase()
+  )
 }
