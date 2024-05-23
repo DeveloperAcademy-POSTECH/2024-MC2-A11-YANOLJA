b2bbf8d29a370bc5ca66aac1cf6b1c72ea5695a3
bono <shippingbok@gmail.com>
2024-05-23 22:27:57 +0900

    [✨Feat] 팀 변경 서비스 추가 / UserDefaults 사용 코드 추가

---
+++ Yanolja/Sources/Domain/UseCase/Interface/SaveMyTeamInterface.swift
@@ -0,0 +1,20 @@
+//
+//  SaveMyTeamInterface.swift
+//  Yanolja
+//
+//  Created by 박혜운 on 5/23/24.
+//  Copyright © 2024 com.mc2.yanolja. All rights reserved.
+//
+
+import Foundation
+
+protocol MyTeamServiceInterface {
+  func readMyTeam() -> BaseballTeam?
+  func saveTeam(to newTeam: BaseballTeam)
+}
+
+extension MyTeamServiceInterface {
+  static func deleteMyTeam() {
+    UserDefaults.standard.removeObject(forKey: "myTeam")
+  }
+}
