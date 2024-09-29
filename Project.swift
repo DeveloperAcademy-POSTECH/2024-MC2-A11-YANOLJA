944d9783c115df0157dc1ee23dd4854da93802c3
bono <shippingbok@gmail.com>
2024-09-29 19:39:59 +0900

    [📚Docs] Tuist Project+Templates에 Config 설정삽입

--- Project.swift
+++ Project.swift
@@ -1,6 +1,5 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
-import MyPlugin
 
 /*
                 +-------------+
@@ -19,9 +18,6 @@ import MyPlugin
  */
 
 // MARK: - Project
-
-// Local plugin loaded
-let localHelper = LocalHelper(name: "MyPlugin")
 
 // Creates our project using a helper function defined in ProjectDescriptionHelpers
 let project = Project.app(
--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -1,25 +1,46 @@
 import ProjectDescription
 import Foundation
+import ConfigPlugin
 
 /// Project helpers are functions that simplify the way you define your project.
 /// Share code to create targets, settings, dependencies,
 /// Create your own conventions, e.g: a func that makes sure all shared targets are "static frameworks"
 /// See https://docs.tuist.io/guides/helpers/
 
+let fileName: String = "Yanolja"
 let teamName: String = "yanolja"
 let yanoljaOrganizationName: String = "com.mc2"
 
 extension Project {
   /// Helper function to create the Project for this ExampleApp
   public static func app(name: String, destinations: Destinations, additionalTargets: [String]) -> Project {
-    var targets = makeAppTargets(name: name,
-                                 destinations: destinations,
-                                 dependencies: additionalTargets.map { TargetDependency.target(name: $0) })
-    targets += additionalTargets.flatMap({ makeFrameworkTargets(name: $0, destinations: destinations) })
+    var targets = makeAppTargets(
+      name: name,
+      destinations: destinations,
+      dependencies: additionalTargets.map { TargetDependency.target(name: $0) }
+    )
+    targets += additionalTargets.flatMap({
+      makeFrameworkTargets(
+        name: $0,
+        destinations: destinations
+      )
+    })
+    
     return Project(
       name: name,
       organizationName: yanoljaOrganizationName,
-      targets: targets
+      settings: .settings(configurations: [
+        .build(.release, name: name),
+        .build(.debug, name: name)
+      ]),
+      targets: targets,
+      schemes: [
+        .makeScheme(.release, name: name),
+        .makeScheme(.debug, name: name)
+      ],
+      additionalFiles: [
+        .sharedConfigPath
+      ]
     )
   }
   
@@ -49,42 +70,48 @@ extension Project {
                        infoPlist: .default,
                        sources: ["\(name)/Tests/**"],
                        resources: [],
-                       dependencies: [.target(name: name)])
+                       dependencies: [.target(name: name)]
+    )
     return [sources, tests]
   }
   
   /// Helper function to create the application target and the unit test target.
   private static func makeAppTargets(name: String, destinations: Destinations, dependencies: [TargetDependency]) -> [Target] {
+    
+    let mainTarget = Target(
+      name: name,
+      platform: .iOS,
+      product: .app,
+      bundleId: "\(yanoljaOrganizationName).\(teamName)",
+      deploymentTarget: .iOS(targetVersion: "17.0", devices: [.iphone]),
       infoPlist: .file(path: "InfoPlist/Info.plist"),
+      sources: ["\(fileName)/Sources/**"],
+      resources: ["\(fileName)/Resources/**"],
+      dependencies: dependencies,
+      settings: .settings(configurations: [
+        .build(.release, name: name),
+        .build(.debug, name: name)
+      ]),
+      coreDataModels: [
+        CoreDataModel("\(fileName)/Sources/Data/Baseball.xcdatamodeld")
       ]
-    ]
-      
-      let mainTarget = Target(
-        name: name,
-        platform: .iOS,
-        product: .app,
-        bundleId: "\(yanoljaOrganizationName).\(teamName)",
-        deploymentTarget: .iOS(targetVersion: "17.0", devices: [.iphone]),
-        sources: ["\(name)/Sources/**"],
-        resources: ["\(name)/Resources/**"],
-        //      scripts: commonScripts,
-        dependencies: dependencies,
-        coreDataModels: [
-          CoreDataModel("\(name)/Sources/Data/Baseball.xcdatamodeld")
-        ]
-      )
-      
-      let testTarget = Target(
-        name: "\(name)Tests",
-        destinations: destinations,
-        product: .unitTests,
-        bundleId: "\(yanoljaOrganizationName).\(name)Tests",
-        sources: ["\(name)/Tests/**"],
-        dependencies: [
-          .target(name: "\(name)")
-        ]
-      )
-      return [mainTarget, testTarget]
-      }
-      }
+    )
+    
+    let testTarget = Target(
+      name: "\(name)Tests",
+      destinations: destinations,
+      product: .unitTests,
+      bundleId: "\(yanoljaOrganizationName).\(teamName)Tests",
       infoPlist: .file(path: "\(fileName)/Tests/Resources/Tests-Info.plist"),
+      sources: ["\(fileName)/Tests/**"],
+      dependencies: [
+        .target(name: "\(name)")
+      ],
+      settings: .settings(configurations: [
+        .build(.release, name: name),
+        .build(.debug, name: name)
+      ])
+    )
+    return [mainTarget, testTarget]
+  }
+}
