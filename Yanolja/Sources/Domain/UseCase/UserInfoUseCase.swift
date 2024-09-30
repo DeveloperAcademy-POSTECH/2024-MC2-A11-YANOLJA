726c25cc1743d34f504f8b2bbcd734138d163ba2
bono <shippingbok@gmail.com>
2024-10-01 00:03:56 +0900

    [✏️Comment] MyTeamUserCase -> UserInfoUserCase 명칭변경

--- Yanolja/Sources/Domain/UseCase/UserInfoUseCase.swift
+++ Yanolja/Sources/Domain/UseCase/UserInfoUseCase.swift
@@ -1,5 +1,5 @@
 //
-//  MyTeamUseCase.swift
+//  userInfoUseCase.swift
 //  Yanolja
 //
 //  Created by 박혜운 on 5/23/24.
@@ -9,28 +9,32 @@
 import SwiftUI
 
 @Observable
-class MyTeamUseCase {
+class UserInfoUseCase {
   struct State {
     var myTeam: BaseballTeam?
+    var myNickname: String = "기본 이름"
   }
   
   enum Action {
     case changeMyTeam(BaseballTeam)
+    case changeMyNickname(String)
   }
   
   private var myTeamService: MyTeamServiceInterface
+  private var myNicknameService: MyNicknameServiceInterface
   private var changeAppIconService: ChangeAppIconInterface
   private var _state: State = .init() // 원본 State, Usecase 내부에서만 접근가능
-  var state: State { // View에서 접근하는 state
-    _state
-  }
+  var state: State { _state } // View에서 접근하는 state
   
   init(
     myTeamService: MyTeamServiceInterface,
+    myNicknameService: MyNicknameServiceInterface,
     changeIconService: ChangeAppIconInterface
   ) {
     self.changeAppIconService = changeIconService
     self.myTeamService = myTeamService
+    self.myNicknameService = myNicknameService
+    _state.myNickname = myNicknameService.readMyNickname() ?? "기본 이름"
     _state.myTeam = myTeamService.readMyTeam()
   }
   
@@ -41,6 +45,11 @@ class MyTeamUseCase {
       myTeamService.saveTeam(to: newTeam)
       changeAppIconService.requestChangeAppIcon(to: newTeam)
       _state.myTeam = newTeam
+      
+    case let .changeMyNickname(newName):
+      myNicknameService.saveNickname(to: newName)
+      _state.myNickname = newName
+      print(newName)
     }
   }
 }
