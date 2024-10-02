b95a83824a3baa687e6caa080c986a003eb6fba1
bono <shippingbok@gmail.com>
2024-10-03 03:29:04 +0900

    [✨Feat] Record 저장,삭제,수정 요청객체 RecordServiceInterface로 변경

--- Yanolja/Sources/Domain/UseCase/RecordUseCase.swift
+++ Yanolja/Sources/Domain/UseCase/RecordUseCase.swift
@@ -17,29 +17,29 @@ class RecordUseCase {
     var editRecordSheet: Bool = false
     
     // MARK: - Data State
-    var recordList: [GameRecordModel] = []
+    var recordList: [GameRecordWithScoreModel] = []
   }
   
   // MARK: - Action
   enum Action {
     case tappedCreateRecordSheet(Bool)
     case tappedRecordCellToEditRecordSheet(Bool)
-    case tappedSaveNewRecord(GameRecordModel)
-    case tappedEditNewRecord(GameRecordModel)
+    case tappedSaveNewRecord(GameRecordWithScoreModel)
+    case tappedEditNewRecord(GameRecordWithScoreModel) 
     case tappedDeleteRecord(UUID)
-//    case tappedChangeTeamRecord(Bool) // 제가 만들엇어요
   }
   
-  private var dataService: DataServiceInterface
+  private var recordService: RecordDataServiceInterface
   private var _state: State = .init() // 실제 원본 State, Usecase 내부에서만 접근가능
   var state: State { // View에서 접근하는 state
     return _state
   }
   
-  init(dataService: DataServiceInterface) {
-    self.dataService = dataService
-    
-    switch dataService.readAllRecord() {
+  init(recordService: RecordDataServiceInterface) {
+    self.recordService = recordService
+    // MARK: - v1.0 기존 Data 값 있다면, 변경해서 저장하고 다시 불러와서 진행
+    // MARK: - v1.0 기존 Data 값 없다면, 정상 진행
+    switch recordService.readAllRecord() {
     case .success(let dataList):
       _state.recordList = dataList
     case .failure:
@@ -59,13 +59,13 @@ class RecordUseCase {
       
       // 데이터 저장 요청
     case let .tappedSaveNewRecord(newRecord):
-      _ = dataService.saveRecord(newRecord)
+      _ = recordService.saveRecord(newRecord)
       _state.recordList.append(newRecord)
       self._state.createRecordSheet = false
       
       // 데이터 수정 요청
     case let .tappedEditNewRecord(editRecord):
-      _ = dataService.editRecord(editRecord)
+      _ = recordService.editRecord(editRecord)
       if let index = _state.recordList.map({ $0.id }).firstIndex(of: editRecord.id) {
         _state.recordList[index] = editRecord
       }
@@ -73,7 +73,7 @@ class RecordUseCase {
       
       // 데이터 삭제 요청
     case let .tappedDeleteRecord(id):
-      _ = dataService.removeRecord(id: id)
+      _ = recordService.removeRecord(id: id)
       _state.recordList = _state.recordList.filter { $0.id != id }
       self._state.editRecordSheet = false
       return
