d1de0a78deed662ebc94db9a03c9e15c13d27dee
bono <shippingbok@gmail.com>
2024-10-03 03:34:04 +0900

    [✨Feat] RecordFilter, YearFilter 배열 생성

---
+++ Yanolja/Sources/Domain/Entity/Common/YearFilter.swift
@@ -0,0 +1,14 @@
+//
+//  YearFilter.swift
+//  Yanolja
+//
+//  Created by 박혜운 on 10/2/24.
+//  Copyright © 2024 com.mc2. All rights reserved.
+//
+
+import Foundation
+
+enum YearFilter {
+  static let list: [String] = ["전체", "2024", "2023"]
+  static let initialValue: String = list.first ?? "전체"
+}
