562fbae8ad772014113b4ccb8e745da091b108a9
bono <shippingbok@gmail.com>
2024-10-03 03:27:04 +0900

    [✨Feat] 메인화면 캐릭터 뷰 수정 및 기초 설정

--- Yanolja/Sources/DesignSystem/Views/Main/MyCharacterView.swift
+++ Yanolja/Sources/DesignSystem/Views/Main/MyCharacterView.swift
@@ -8,138 +8,42 @@
 
 import SwiftUI
 
-struct ShakingImageView: View {
-  @State private var isShaking = false
-  @State private var timer: Timer?
-  var image: Image
-  
-  var body: some View {
-    image
-      .resizable()
-      .scaledToFit()
-      .aspectRatio(contentMode: .fit)
-      .offset(x: isShaking ? -10 : 0)
-      .animation(
-        isShaking ? Animation
-          .linear(duration: 0.05)
-          .repeatForever(autoreverses: true) :
-          .default, value: isShaking
-      )
-      .onAppear {
-        startShaking()
-      }
-      .onDisappear {
-        stopShaking()
-      }
-  }
-  
-  private func startShaking() {
-    timer = Timer.scheduledTimer(withTimeInterval: 1.3, repeats: true) { _ in
-      withAnimation {
-        isShaking.toggle()
-      }
-      
-      DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-        withAnimation {
-          isShaking.toggle()
-        }
-      }
-    }
-  }
-  
-  private func stopShaking() {
-    timer?.invalidate()
-    timer = nil
-  }
-}
-
-struct BouncingImageView: View {
-  @State private var isBouncing = false
-  var image: Image
-  
-  var body: some View {
-    image
-      .resizable()
-      .scaledToFit()
-      .aspectRatio(contentMode: .fit)
-      .offset(y: isBouncing ? -20 : 35)
-      .animation(
-        Animation.easeInOut(duration: 1.4)
-          .repeatForever(autoreverses: true),
-        value: isBouncing
-      )
-      .onAppear {
-        withAnimation {
-          isBouncing.toggle()
-        }
-      }
-  }
-}
-
-struct ScalingImageView: View {
-  @State private var isScaled = false
-  var image: Image
-  
-  var body: some View {
-    image
-      .resizable()
-      .scaledToFit()
-      .scaleEffect(isScaled ? 1.07 : 1.0)
-      .onAppear {
-        withAnimation(Animation.easeInOut(duration: 1.4).repeatForever(autoreverses: true)) {
-          isScaled.toggle()
-        }
-      }
-  }
-}
-
-struct HappyImageView: View {
-  @State private var isHappy = false
-  var image: Image
-  
-  var body: some View {
-    image
-      .resizable()
-      .scaledToFit()
-      .rotationEffect(.degrees(isHappy ? 15 : -15))
-      .scaleEffect(isHappy ? 1.08 : 1.0)
-      .onAppear {
-        withAnimation(Animation.easeInOut(duration: 1.5).repeatForever(autoreverses: true)) {
-          isHappy.toggle()
-        }
-      }
-  }
-}
-
-struct MyCharacterView: View {
+struct MainCharacterView: View {
   let characterModel: CharacterModel
   
   var body: some View {
     VStack {
-      BubbleTextView(
-        text: characterModel.message
-      )
-      
-      if characterModel.emotionByWinRate == .veryBad {
-        ScalingImageView(
-          image: characterModel.image
-        )
-      } else if characterModel.emotionByWinRate == .bad 
-      {
-        ShakingImageView(image: characterModel.image)
-      } else if characterModel.emotionByWinRate == .excellent {
-        HappyImageView(image: characterModel.image)
-      } else {
-        BouncingImageView(image: characterModel.image)
+      Group {
+        switch characterModel.emotionByWinRate {
+        case .none:
+          VStack {
+            Image(.mainCharacter0Line)
+              .renderingMode(.template)
+            Image(.shadow)
+          }
+        case .veryBad:
+          Image(.mainCharacter1Line)
+            .renderingMode(.template)
+        case .bad:
+          Image(.mainCharacter2Line)
+            .renderingMode(.template)
+        case .good:
+          Image(.mainCharacter3Line)
+            .renderingMode(.template)
+        case .excellent:
+          Image(.mainCharacter4Line)
+            .renderingMode(.template)
+        }
       }
+      .foregroundColor(characterModel.myTeam?.mainColor ?? .none1)
     }
   }
 }
 
 
 #Preview {
-  MyCharacterView(
-    characterModel: .init(myTeam: .kiwoom, totalWinRate: 50)
+  MainCharacterView(
+    characterModel: .init(myTeam: .kiwoom, totalWinRate: 0)
   )
 }
 
