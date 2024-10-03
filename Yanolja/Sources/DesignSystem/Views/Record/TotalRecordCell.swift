c35bcf3cb6b49c3302f3649af749595fc820576e
wltnryu <yebbrnjs@gmail.com>
2024-10-03 16:05:51 +0900

    [💄UI] RecordCell UI 변경 및 점선 추가 + AllRecordView UI 변경 및 버튼 추가

---
+++ Yanolja/Sources/DesignSystem/Views/Record/TotalRecordCell.swift
@@ -0,0 +1,19 @@
+//
+//  TotalRecordCell.swift
+//  Yanolja
+//
+//  Created by 유지수 on 10/3/24.
+//  Copyright © 2024 com.mc2. All rights reserved.
+//
+
+import SwiftUI
+
+struct TotalRecordCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TotalRecordCell()
+}
