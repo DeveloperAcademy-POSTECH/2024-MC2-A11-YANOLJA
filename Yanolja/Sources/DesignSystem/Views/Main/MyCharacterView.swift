28b136d9c6c254ce6e30fe3cc8065b6cce2a3b17
bono <shippingbok@gmail.com>
2024-10-05 04:03:22 +0900

    [💄UI] Main 화면 캐릭터 이미지 겹치기 완료 MyCharacterView

--- Yanolja/Sources/DesignSystem/Views/Main/MyCharacterView.swift
+++ Yanolja/Sources/DesignSystem/Views/Main/MyCharacterView.swift
@@ -13,26 +13,38 @@ struct MainCharacterView: View {
   
   var body: some View {
     VStack {
-      Group {
+      ZStack {
         switch characterModel.emotionByWinRate {
         case .none:
+          Image(.mainCharacter0Background)
+          Image(.mainCharacter0Face)
+            .renderingMode(.template)
+          Image(.mainCharacter0Line)
           VStack {
-            Image(.mainCharacter0Line)
-              .renderingMode(.template)
-            Image(.shadow)
+            Spacer()
+            Image(.mainCharacter0Shadow)
           }
+          .frame(height: 280)
         case .veryBad:
+          Image(.mainCharacter1Background)
+          Image(.mainCharacter1Face)
+            .renderingMode(.template)
           Image(.mainCharacter1Line)
-            .renderingMode(.template)
         case .bad:
+          Image(.mainCharacter2Background)
+          Image(.mainCharacter2Face)
+            .renderingMode(.template)
           Image(.mainCharacter2Line)
-            .renderingMode(.template)
         case .good:
+          Image(.mainCharacter3Background)
+          Image(.mainCharacter3Face)
+            .renderingMode(.template)
           Image(.mainCharacter3Line)
-            .renderingMode(.template)
         case .excellent:
-          Image(.mainCharacter4Line)
+          Image(.mainCharacter4Background)
+          Image(.mainCharacter4Face)
             .renderingMode(.template)
+          Image(.mainCharacter4Line)
         }
       }
       .foregroundColor(characterModel.myTeam?.mainColor ?? .none1)
@@ -43,7 +55,7 @@ struct MainCharacterView: View {
 
 #Preview {
   MainCharacterView(
-    characterModel: .init(myTeam: .kiwoom, totalWinRate: 0)
+    characterModel: .init(myTeam: .kiwoom, totalWinRate: 100)
   )
 }
 
