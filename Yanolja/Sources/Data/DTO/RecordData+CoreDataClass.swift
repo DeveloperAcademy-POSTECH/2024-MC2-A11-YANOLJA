51d30d95a4996fa27fc67d062a7561986968f9a9
장해원/에디(Eddie) <145424895+penum1227@users.noreply.github.com>
2024-10-08 02:33:07 +0900

    [✨Feat]  레코드 데이터 엔티티 형성 및 서브 클래스 참조 설정 및 새로운 코어 데이터 로직 추가

---
+++ Yanolja/Sources/Data/DTO/RecordData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  RecordData+CoreDataClass.swift
+//  
+//
+//  Created by jhon on 10/8/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(RecordData)
+public class RecordData: NSManagedObject {
+
+}
