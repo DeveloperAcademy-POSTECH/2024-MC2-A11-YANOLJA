ea7e94e6e208bc3aefb5a70d165a357444e4c380
bono <shippingbok@gmail.com>
2024-10-08 19:08:14 +0900

    [✨Feat] WinRateUseCase에서 구단별 정보 관리 객체 생성 및 관리로직 수정 / 기존 winCount loseCount drawCount 삭제

--- Yanolja/Sources/Domain/UseCase/WinRateUseCase.swift
+++ Yanolja/Sources/Domain/UseCase/WinRateUseCase.swift
@@ -18,10 +18,9 @@ class WinRateUseCase {
     
     // MARK: - Data State
     var myTeam: BaseballTeam = .noTeam
-    var myWinRate: WinRateModel = .init(totalWinRate: 40)
-    var myWinCount: Int = 0
-    var myLoseCount: Int = 0
-    var myDrawCount: Int = 0
+    var totalWinRate: Int? = 88
+    var eachTeamAnalyze: EachTeamAnalyzeModel = .init()
+    var eachStadiumsAnalyze: EachStadiumsAnalyzeModel = .init()
     fileprivate var recordList: [GameRecordWithScoreModel] = []
     var filteredRecordList: [GameRecordWithScoreModel] {
       self.recordList.filtered(years: selectedYearFilter)
@@ -54,7 +53,7 @@ class WinRateUseCase {
       self._state.recordList = allList
       self.totalWinRate(recordList: allList)
       self.vsAllTeamWinRate(recordList: allList)
-      self.allRecordResult(recordList: allList)
+      self.vsAllStadiumsWinRate(recordList: allList)
       
     case .failure:
       break
@@ -71,7 +70,7 @@ class WinRateUseCase {
     _state.recordList = recordList
     self.totalWinRate(recordList: recordList)
     self.vsAllTeamWinRate(recordList: recordList)
-    self.allRecordResult(recordList: recordList)
+    self.vsAllStadiumsWinRate(recordList: recordList)
   }
   
   func effect(_ action: Action) {
@@ -82,6 +81,9 @@ class WinRateUseCase {
     case let .tappedAnalyzeYearFilter(year): // 연도 정보 토대로 필터정보 변경
       _state.selectedYearFilter = year
       let recordList = _state.recordList.filtered(years: year)
+      self.vsAllTeamWinRate(recordList: recordList)
+      self.vsAllStadiumsWinRate(recordList: recordList)
+      
     case .tappedTeamWinRateCell:
       _state.tappedTeamWinRateCell.toggle()
       
@@ -89,7 +91,7 @@ class WinRateUseCase {
       self._state.recordList = records
       self.totalWinRate(recordList: records)
       self.vsAllTeamWinRate(recordList: records)
-      self.allRecordResult(recordList: records)
+      self.vsAllStadiumsWinRate(recordList: records)
       
     case let .sendMyTeamInfo(myTeam):
       _state.myTeam = myTeam
@@ -100,29 +102,31 @@ class WinRateUseCase {
   /// 직관 기록을 통해 총 승률을 계산하고 입력합니다
   private func totalWinRate(recordList: [GameRecordWithScoreModel])  {
     
-    let totalGames = recordList.count // 무승부를 포함한 전체 게임 수
-    let drawCount = recordList.filter{ $0.result == .draw}.count // 무승부 수
-    let winCount = recordList.filter{ $0.result == .win}.count // 이긴 게임 수
-    let winRateGames = totalGames - drawCount // 무승부를 제외한 전체 게임 수
-    _state.myWinRate.totalRecordCount = winRateGames
+    let totalGames = recordList.filter { $0.result != .cancel }.count // 취소를 제외한 경기 중 무승부를 포함한 전체 게임 수
+    let drawCount = recordList.filter { $0.result == .draw}.count // 무승부 수
+    let winCount = recordList.filter { $0.result == .win}.count // 이긴 게임 수
+    let winOrLoseGamesCount = totalGames - drawCount // 무승부를 제외한 전체 게임 수
     
-    guard winRateGames > 0 else {
-      _state.myWinRate.totalWinRate = nil
-      return
+    if winOrLoseGamesCount > 0 {
+      _state.totalWinRate = Int(Double(winCount) / Double(winOrLoseGamesCount) * 100)
+    } else {
+      _state.totalWinRate = nil
     }
-    let winRate = Int(Double(winCount) / Double(winRateGames) * 100)
-    _state.myWinRate.totalWinRate = winRate
   }
   /// 직관 기록을 통해 구단 별 승률을 계산하고 입력합니다
   private func vsAllTeamWinRate(recordList: [GameRecordWithScoreModel]) {
     var teamWins: [BaseballTeam: Int] = [:]
-    var teamGames: [BaseballTeam: Int] = [:]
+    var teamGamesAll: [BaseballTeam: Int] = [:]
+    var teamGamesNotCancel: [BaseballTeam: Int] = [:]
     var teamDraws: [BaseballTeam: Int] = [:]
+    let recordList = recordList.filtered(years: state.selectedYearFilter)
     
     for record in recordList {
       let team = record.vsTeam
+      teamGamesAll[team, default: 0] += 1
       
-      teamGames[team, default: 0] += 1
+      guard record.result != .cancel else { continue } // 취소 경기는 포함하지 않음
+      teamGamesNotCancel[team, default: 0] += 1
       
       if record.result == .win {
         // 승리한 경우에만 승리 수 증가
@@ -139,41 +143,68 @@ class WinRateUseCase {
     var vsTeamWinRate: [BaseballTeam: Int?] = [:]
     var vsTeamRecordCount: [BaseballTeam: Int?] = [:]
     
-    for team in BaseballTeam.allCases {
+    for team in BaseballTeam.recordBaseBallTeam {
       let wins = teamWins[team] ?? 0
-      let games = teamGames[team] ?? 0
+      let gamesNotCancel = teamGamesNotCancel[team] ?? 0
+      let gamesAll = teamGamesAll[team]
       let draws = teamDraws[team] ?? 0
       
-      vsTeamRecordCount[team] = games
+      vsTeamRecordCount[team] = gamesAll
       
-      if games-draws > 0 {
-        vsTeamWinRate[team] = Int((Double(wins) / Double(games-draws)) * 100)
+      if gamesNotCancel-draws > 0 {
+        vsTeamWinRate[team] = Int((Double(wins) / Double(gamesNotCancel-draws)) * 100)
       }
     }
     
-    _state.myWinRate.vsTeamWinRate = vsTeamWinRate
-    _state.myWinRate.vsTeamRecordCount = vsTeamRecordCount
+    _state.eachTeamAnalyze.vsTeamWinRate = vsTeamWinRate
+    _state.eachTeamAnalyze.vsTeamRecordCount = vsTeamRecordCount
   }
   
-  /// 직관 기록을 통해 승, 패, 무 숫자를 갱신합니다
-  private func allRecordResult(recordList: [GameRecordWithScoreModel]) {
-    var winCount: Int = 0
-    var loseCount: Int = 0
-    var drawCount: Int = 0
+  /// 직관 기록을 통해 구장 별 승률을 계산하고 입력합니다
+  private func vsAllStadiumsWinRate(recordList: [GameRecordWithScoreModel]) {
+    var stadiumsWins: [String: Int] = [:]
+    var stadiumsGamesAll: [String: Int] = [:]
+    var stadiumsGamesNotCancel: [String: Int] = [:]
+    var stadiumsDraws: [String: Int] = [:]
+    let recordList = recordList.filtered(years: state.selectedYearFilter)
+    
     for record in recordList {
-      guard !record.isCancel else { continue }
-      switch record.result {
-      case .win:
-        winCount += 1
-      case .lose:
-        loseCount += 1
-      case .draw:
-        drawCount += 1
-      case .cancel: continue
+      let stadiums = record.stadiums
+      stadiumsGamesAll[stadiums, default: 0] += 1
+      
+      guard record.result != .cancel else { continue } // 취소 경기는 포함하지 않음
+
+      stadiumsGamesNotCancel[stadiums, default: 0] += 1
+      
+      if record.result == .win {
+        // 승리한 경우에만 승리 수 증가
+        stadiumsWins[stadiums, default: 0] += 1
+      }
+      
+      if record.result == .draw {
+        // 무승부인 경우 무승부 수 증가
+        stadiumsDraws[stadiums, default: 0] += 1
       }
     }
-    _state.myWinCount = winCount
-    _state.myLoseCount = loseCount
-    _state.myDrawCount = drawCount
+    
+    // 각 팀의 승률 계산 및 상태에 저장
+    var stadiumsWinRate: [String: Int?] = [:]
+    var stadiumsRecordCount: [String: Int?] = [:]
+    
+    for stadiums in BaseballStadiums.nameList {
+      let wins = stadiumsWins[stadiums] ?? 0
+      let gamesNotCancel = stadiumsGamesNotCancel[stadiums] ?? 0
+      let gamesAll = stadiumsGamesAll[stadiums]
+      let draws = stadiumsDraws[stadiums] ?? 0
+      
+      stadiumsRecordCount[stadiums] = gamesAll
+      
+      if gamesNotCancel-draws > 0 {
+        stadiumsWinRate[stadiums] = Int((Double(wins) / Double(gamesNotCancel-draws)) * 100)
+      }
+    }
+    
+    _state.eachStadiumsAnalyze.stadiumsWinRate = stadiumsWinRate
+    _state.eachStadiumsAnalyze.stadiumsRecordCount = stadiumsRecordCount
   }
 }
