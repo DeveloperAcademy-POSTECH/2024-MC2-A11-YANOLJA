5d29c38db697516e59587925f870e0eebf77c688
bono <shippingbok@gmail.com>
2024-10-13 03:46:53 +0900

    [✨Feat] BaseballStadiums convertName 생성, nameList 상수 -> 변수
    기존 기록 불러와 전환 시 필요한 계산속성 생성, 통신 후 nameList 주입 위해 변수로 변경

--- Yanolja/Sources/Domain/Entity/Common/BaseballStadiums.swift
+++ Yanolja/Sources/Domain/Entity/Common/BaseballStadiums.swift
@@ -9,7 +9,8 @@
 import Foundation
 
 enum BaseballStadiums: String, CaseIterable {
-  static let nameList: [String] = ["고척스카이돔", "사직야구장", "잠실야구장", "수원KT위즈파크", "인천SSG랜더스필드", "대전한화생명이글스파크", "대구삼성라이온즈파크", "창원NC파크", "광주기아챔피언스필드", "포항야구장"]
+  static var nameList: [String] = ["잠실종합운동장야구장", "인천SSG랜더스필드", "울산문수야구장", "대구삼성라이온즈파크", "창원NC파크", "부산사직야구장", "광주KIA챔피언스필드", "고척스카이돔", "포항야구장", "대전한화생명이글스파크", "수원KT위즈파크"]
+  
   
   case gocheok
   case sagik
@@ -42,6 +43,31 @@ enum BaseballStadiums: String, CaseIterable {
       return "창원NC파크"
     case .gwangju:
       return "광주기아챔피언스필드"
+    case .pohang:
+      return "포항야구장"
+    }
+  }
+  
+  var convertName: String {
+    switch self {
+    case .gocheok:
+      return "고척스카이돔"
+    case .sagik:
+      return "부산사직야구장"
+    case .jamsil:
+      return "잠실종합운동장야구장"
+    case .suwon:
+      return "수원KT위즈파크"
+    case .incheon:
+      return "인천SSG랜더스필드"
+    case .daejeon:
+      return "대전한화생명이글스파크"
+    case .daegu:
+      return "대구삼성라이온즈파크"
+    case .changwon:
+      return "창원NC파크"
+    case .gwangju:
+      return "광주KIA챔피언스필드"
     case .pohang:
       return "포항야구장"
     }
