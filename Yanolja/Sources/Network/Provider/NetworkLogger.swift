e652d406318faa886f5f489e814f1fc198c90fdc
bono <shippingbok@gmail.com>
2024-10-13 11:32:42 +0900

    [✨Feat] Release 상황에서 Print문 생략

--- Yanolja/Sources/Network/Provider/NetworkLogger.swift
+++ Yanolja/Sources/Network/Provider/NetworkLogger.swift
@@ -92,17 +92,3 @@ fileprivate extension Data {
     return prettyPrintedString as String
   }
 }
-
-/// RELEASE 모드에서 프린트 되지 않게 print()에 대한 오버라이딩
-fileprivate func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-#if DEBUG
-  Swift.print(items, separator: separator, terminator: terminator)
-#endif
-}
-
-/// RELEASE 모드에서 프린트 되지 않게 debugPrint()에 대한 오버라이딩
-fileprivate func debugPrint(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-#if DEBUG
-  Swift.debugPrint(items, separator: separator, terminator: terminator)
-#endif
-}
