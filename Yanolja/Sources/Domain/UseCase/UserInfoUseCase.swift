3bf4264efdab6e6db873a919fa8a73211f0f42df
bono <shippingbok@gmail.com>
2024-10-13 11:49:13 +0900

    [🔥HOTFIX] BubbleTexts 무직고려 후 로직 수정

--- Yanolja/Sources/Domain/UseCase/UserInfoUseCase.swift
+++ Yanolja/Sources/Domain/UseCase/UserInfoUseCase.swift
@@ -13,38 +13,53 @@ class UserInfoUseCase {
   struct State {
     var myTeam: BaseballTeam?
     var myNickname: String?
+    var bubbleTextList: [String] = []
   }
   
   enum Action {
     case changeMyTeam(BaseballTeam)
     case changeMyNickname(String)
+    case setBubbleTexts
   }
   
   private var myTeamService: MyTeamServiceInterface
   private var myNicknameService: MyNicknameServiceInterface
   private var changeAppIconService: ChangeAppIconInterface
+  private var settingsService: SettingsService
   private var _state: State = .init() // 원본 State, Usecase 내부에서만 접근가능
   var state: State { _state } // View에서 접근하는 state
   
   init(
     myTeamService: MyTeamServiceInterface,
     myNicknameService: MyNicknameServiceInterface,
-    changeIconService: ChangeAppIconInterface
+    changeIconService: ChangeAppIconInterface,
+    settingsService: SettingsService
   ) {
     self.changeAppIconService = changeIconService
     self.myTeamService = myTeamService
     self.myNicknameService = myNicknameService
+    self.settingsService = settingsService
     _state.myNickname = myNicknameService.readMyNickname()
     _state.myTeam = myTeamService.readMyTeam()
+    self.effect(.setBubbleTexts)
   }
   
   // MARK: - View Action
   func effect(_ action: Action) {
     switch action {
+      
+    case .setBubbleTexts:
+      Task {
+        if case let .success(bubbleTextList) = await settingsService.characterBubbleTexts(_state.myTeam?.sliceName ?? "두산") {
+          _state.bubbleTextList = bubbleTextList
+        }
+      }
+      
     case let .changeMyTeam(newTeam):
       myTeamService.saveTeam(to: newTeam)
       changeAppIconService.requestChangeAppIcon(to: newTeam)
       _state.myTeam = newTeam
+      self.effect(.setBubbleTexts)
       
     case let .changeMyNickname(newName):
       myNicknameService.saveNickname(to: newName)
