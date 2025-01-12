0d8c447bf16cb6fa99a530cef52191df42e5c568
bono <shippingbok@gmail.com>
2025-01-12 21:58:28 +0900

    [✏️Comment] 커식을 위한 사소한 수정

--- Yanolja/Sources/Screens/Analyze/AllAnalyticsView.swift
+++ Yanolja/Sources/Screens/Analyze/AllAnalyticsView.swift
@@ -3,8 +3,8 @@
 //  Yanolja
 //
 //  Created by 박혜운 on 9/29/24.
-//  Copyright © 2024 com.mc2. All rights reserved. 
-//
+//  Copyright © 2024 com.mc2. All rights reserved.
+//   
 
 import SwiftUI
 
