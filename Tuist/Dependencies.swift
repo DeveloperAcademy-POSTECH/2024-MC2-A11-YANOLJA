40ee63e7991eddfa050591f2597fe5e19893bb29
bono <shippingbok@gmail.com>
2025-01-13 15:39:55 +0900

    [feat] Tusit Mixpanel SPM 의존성 추가

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -6,3 +6,9 @@
 //
 
 @preconcurrency import ProjectDescription
+
+let dependencies = Dependencies(
+  swiftPackageManager: .init([
+    .package(url: "https://github.com/mixpanel/mixpanel-swift.git", from: "3.1.0")
+  ])
+)
