ba861ff0f3f4d766f81ac64a765051418b708115
bono <shippingbok@gmail.com>
2025-03-14 02:13:18 +0900

    [feat] 불변 규칙 저장 Entity 생성 - KeepingWinningRule

---
+++ KeepingWinner/Sources/Domain/Model/Entity/KeepingWinningRule.swift
@@ -0,0 +1,14 @@
+//
+//  KeepingWinningRule.swift
+//  KeepingWinner
+//
+//  Created by 박혜운 on 3/1/25.
+//
+
+import Foundation
+
+enum KeepingWinningRule {
+  static let dataLimitYear: Int = 2015
+  static let dataUpdateYear: Int = 2025
+  static let defaultBubbleTexts: [String] = ["오늘은 누구 응원할까?", "내가 혹시 승리요정??", "어느 구단으로 취직하지?", "이기는 팀 우리 팀?", "홈런 가자!?"]
+}
